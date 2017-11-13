d39de61bd41a0325fdba05bff0d204e422c70f43
Vishal <uppalvishal@gmail.com>
2017-11-13 06:37:33 +0530

    Added production web server express

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
-const path= require('path')
-module.exports = {
+const path= require('path');
+
+module.exports =(env) =>{
+ const isProduction=env=='production';
+    return {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname,'public'),
@@ -19,8 +22,9 @@ module.exports = {
             ]
         }]
     },
-    devtool:'cheap-module-eval-source-map',
+    devtool:isProduction ? 'source-map':'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname,'public')
     }
-};
+ };
+ };
